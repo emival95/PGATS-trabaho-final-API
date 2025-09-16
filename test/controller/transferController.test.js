8fd574b1f7a54f7f103175ff9b4d6ab4feb39795
Emival Ferreira Barros Filho <re048522@qintess.com>
2025-09-16 18:16:55 -0300

    adicionando teste de api

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -5,7 +5,7 @@ const { expect } = require('chai');
 
 
 const app = require('../../app');
-const { from } = require('form-data');
+const transferService = require('../../service/transferService');
 
 
 describe('Transfer Controller', () => {
@@ -22,9 +22,49 @@ describe('Transfer Controller', () => {
                  expect(resposta.body).to.have.property('error', 'Usuário não encontrado')
             });
             
-            describe('GET /transfers', () => {
+            
+           it('Usando mocks: Quando informo remetente e destinatario inexistente encontrado', async () => {
+            
+            const transferServicemock = sinon.stub(transferService,'transfer')
+            transferServicemock.throws(new Error('Usuário não encontrado'));
 
-            })
+            const resposta = await request(app)
+                 .post('/transfers')
+                 .send({
+                     from: 'Emival',
+                     to: 'Miriam',
+                     value: 100
+                 });
+                 expect(resposta.status).to.equal(400);
+                 expect(resposta.body).to.have.property('error', 'Usuário não encontrado')
+
+                 sinon.restore();
+            });
+
+             it('Usando mocks: Quando informo valores valido tenho sucesso', async () => {
+            
+            const transferServicemock = sinon.stub(transferService,'transfer')
+            transferServicemock.returns({ 
+                from: 'Emival',
+                to:'Miriam',
+                value:100,
+                date: new Date().toISOString()
+            });
+
+            const resposta = await request(app)
+                 .post('/transfers')
+                 .send({
+                     from: 'Emival',
+                     to: 'Miriam',
+                     value: 100
+                 });
+                 expect(resposta.status).to.equal(201);
+                 expect(resposta.body).to.have.property('from', 'Emival')
+                 expect(resposta.body).to.have.property('to', 'Miriam')
+                 expect(resposta.body).to.have.property('value', 100)
+
+                 sinon.restore();
+            });
                 
                    
             
